42c2a5a0326e55664f7809d8c37c4b9062f99fef
Edel Dsouza <edeldsouza160@gmail.com>
2025-08-05 00:11:53 +0530

    Initial commit

---
+++ Frontend/Expense-tracker/src/pages/Dashboard/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import DashboardLayout from '../../Components/layouts/DashboardLayout'
+
+const Home = () => {
+  return (
+    <DashboardLayout activeMenu = "Dashboard">
+    <div className="my-5 mx-auto">
+      
+    </div>
+    </DashboardLayout>
+  )
+}
+
+export default Home
